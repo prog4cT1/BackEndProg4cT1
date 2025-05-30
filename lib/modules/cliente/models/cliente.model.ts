7b151e6f68f0746f557bd88a85629cecd8018bf6
prog4cT1 Cecyte <prog4ct1@gmail.com>
2025-05-30 20:35:29 +0000

    Mi cambio

---
+++ lib/modules/cliente/models/cliente.model.ts
@@ -0,0 +1,30 @@
+
+import { ROLES } from '../../../config';
+import * as mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+
+
+export interface ICliente extends mongoose.Document {  
+    cliente: string;
+    telefono: string;
+ }
+
+ const ClienteSchema = new Schema(
+    {
+        cliente: {
+            type: String,
+            required: [true, 'cliente required'],
+            unique: true
+        },
+        telefono: {
+             type: String,
+            required: [true, 'telefono required']
+        }
+    }
+    
+ );
+
+const Cliente = mongoose.model<ICliente>("Cliente", ClienteSchema);
+export default Cliente;
